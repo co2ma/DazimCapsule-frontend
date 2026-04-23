db5e4bee55b4c07353dda108637f230b4fabe978
Co2ma <window3357@gmail.com>
2026-04-23 21:55:53 +0900

    feat: 내용 멘트 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,45 +1,46 @@
-import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
-import { Analytics } from '@vercel/analytics/next'
-import './globals.css'
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
+import "./globals.css";
 
 const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: '별빛 아래, 꿈을 그리다',
-  description: '어둠 속에서도 빛나는 별처럼, 당신의 이야기가 시작되는 곳',
-  generator: 'v0.app',
-  icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
-  },
-}
+    title: "다짐 캡슐",
+    description:
+        "마음속으로만 했던 다짐, 전하지 못했던 진심, 이루고자 하는 목표. 이곳 다짐 캡슐에서 편하게 이야기 해보아요.",
+    generator: "v0.app",
+    icons: {
+        icon: [
+            {
+                url: "/icon-light-32x32.png",
+                media: "(prefers-color-scheme: light)",
+            },
+            {
+                url: "/icon-dark-32x32.png",
+                media: "(prefers-color-scheme: dark)",
+            },
+            {
+                url: "/icon.svg",
+                type: "image/svg+xml",
+            },
+        ],
+        apple: "/apple-icon.png",
+    },
+};
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className="font-sans antialiased">
-        {children}
-        {process.env.NODE_ENV === 'production' && <Analytics />}
-      </body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body className="font-sans antialiased">
+                {children}
+                {process.env.NODE_ENV === "production" && <Analytics />}
+            </body>
+        </html>
+    );
 }
