8fa5f54153f48ab577e122ca65387aa8c0936671
Co2ma <window3357@gmail.com>
2026-04-26 02:20:53 +0900

    fix: middleware.ts 에서 proxy.ts로 변경
    middleware 방식이 Express.js 미들웨어와 혼동될 가능성이 있어서 proxy로 이름이 변경됨

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
     const pathname = request.nextUrl.pathname;
 
     // /form/으로 시작하는 경로만 검사
